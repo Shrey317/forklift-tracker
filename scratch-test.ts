c48df178f12fc87240cc24bb0a22c855c52d8591
Shrey317 <drash1731@gmail.com>
2026-09-10 23:50:02 +0200

    Fix dashboard layout provider and remove scratch files

--- scratch-test.ts
+++
@@ -1,6 +0,0 @@
-import { parseScannedQrContent } from './src/lib/qr-parse.ts';
-
-console.log('Test 1:', parseScannedQrContent('http://localhost:3000/forklift/some-token'));
-console.log('Test 2:', parseScannedQrContent('http://192.168.1.10:3000/forklift/another-token'));
-console.log('Test 3:', parseScannedQrContent('/forklift/just-path-token'));
-console.log('Test 4:', parseScannedQrContent('some-token'));
